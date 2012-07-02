51648790d993cdd1f907b7d61fc1ecd9aead084b
Jessie Taylor <b3457m0d3@interr0bang.net>
2012-07-02 00:29:53 -0300

    CodeIgniter-ready Version

--- example.php
+++ example.php
@@ -1,19 +1,19 @@
 <?php
-
+// JWT.php goes in application/libraries
 //example controller function 
-// for use with Annotator Authentication (https://github.com/okfn/annotator)
+// test case: Annotator Authentication (https://github.com/okfn/annotator)
 
 public function generate_token($user_id){
-     $this->load->library("JWT");
-     $CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-	    $CONSUMER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-	    $CONSUMER_TTL = 86400;
-	    echo $this->jwt->encode(array(
-	      'consumerKey'=>$CONSUMER_KEY,
-	      'userId'=>$user_id,
-	      'issuedAt'=>date(DATE_ISO8601, strtotime("now")),
-	      'ttl'=>$CONSUMER_TTL
-	    ), $CONSUMER_SECRET);
-	}
+    $this->load->library("JWT");
+    $CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    $CONSUMER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    $CONSUMER_TTL = 86400;
+    echo $this->jwt->encode(array(
+      'consumerKey'=>$CONSUMER_KEY,
+      'userId'=>$user_id,
+      'issuedAt'=>date(DATE_ISO8601, strtotime("now")),
+      'ttl'=>$CONSUMER_TTL
+    ), $CONSUMER_SECRET);
+}
 
 ?>
