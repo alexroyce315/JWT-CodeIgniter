fce024e285b1e9b785cca4956034d23aebaf80d3
Alex Royce <alexroyce315@users.noreply.github.com>
2017-05-18 17:35:25 +0800

    make one single file.

--- example.php
+++ example.php
@@ -1,19 +1,16 @@
-<?php
-// JWT.php goes in application/libraries
-//example controller function 
-// test case: Annotator Authentication (https://github.com/okfn/annotator)
+<?php defined('BASEPATH') OR ('No direct script access allowed');
 
-public function generate_token($user_id){
-    $this->load->library("JWT");
-    $CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-    $CONSUMER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-    $CONSUMER_TTL = 86400;
-    echo $this->jwt->encode(array(
-      'consumerKey'=>$CONSUMER_KEY,
-      'userId'=>$user_id,
-      'issuedAt'=>date(DATE_ISO8601, strtotime("now")),
-      'ttl'=>$CONSUMER_TTL
-    ), $CONSUMER_SECRET);
+class Example extends MA_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        log_message('info', 'Example Class Initialized');
+    }
+    
+    public function token($userId = 'demo') {
+        $this->load->library('Jwt');
+        echo $this->jwt->encode(array(
+            'userId'  => $userId
+        ));
+    }
 }
-
-?>
